89cc4b3fbab26a6f3c9e762833c10773845b83c2
KildrenVX <migux07@gmail.com>
2017-05-05 12:33:30 -0400

    Ventana de registro, hay unos problemas en el layaout de Login

--- app/src/main/java/com/example/m/meba_v2/View/Registro_User.java
+++ app/src/main/java/com/example/m/meba_v2/View/Registro_User.java
@@ -1,7 +1,9 @@
-package com.example.m.meba_v2;
+package com.example.m.meba_v2.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.m.meba_v2.R;
 
 public class Registro_User extends AppCompatActivity  {
 
