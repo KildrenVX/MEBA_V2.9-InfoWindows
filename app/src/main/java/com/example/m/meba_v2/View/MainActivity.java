94f250de5641ae8016ef3284d5efe819ede05193
KildrenVX <migux07@gmail.com>
2017-05-07 21:45:00 -0300

    alert dialog para agregar marcadores

--- app/src/main/java/com/example/m/meba_v2/View/MainActivity.java
+++ app/src/main/java/com/example/m/meba_v2/View/MainActivity.java
@@ -1,40 +1,43 @@
  package com.example.m.meba_v2.View;
 
  import android.Manifest;
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.content.ContextCompat;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.Toast;
+ import android.app.AlertDialog;
+ import android.content.Context;
+ import android.content.DialogInterface;
+ import android.content.Intent;
+ import android.content.pm.PackageManager;
+ import android.location.Location;
+ import android.location.LocationListener;
+ import android.location.LocationManager;
+ import android.os.Bundle;
+ import android.support.annotation.NonNull;
+ import android.support.annotation.Nullable;
+ import android.support.v4.app.ActivityCompat;
+ import android.support.v4.app.FragmentActivity;
+ import android.support.v4.content.ContextCompat;
+ import android.util.Log;
+ import android.view.LayoutInflater;
+ import android.view.View;
+ import android.widget.Toast;
 
-import com.example.m.meba_v2.R;
-import com.getbase.floatingactionbutton.FloatingActionButton;
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.maps.CameraUpdate;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
+ import com.example.m.meba_v2.R;
+ import com.getbase.floatingactionbutton.FloatingActionButton;
+ import com.google.android.gms.common.ConnectionResult;
+ import com.google.android.gms.common.api.GoogleApiClient;
+ import com.google.android.gms.maps.CameraUpdate;
+ import com.google.android.gms.maps.CameraUpdateFactory;
+ import com.google.android.gms.maps.GoogleMap;
+ import com.google.android.gms.maps.OnMapReadyCallback;
+ import com.google.android.gms.maps.SupportMapFragment;
+ import com.google.android.gms.maps.model.LatLng;
+ import com.google.android.gms.maps.model.Marker;
+ import com.google.android.gms.maps.model.MarkerOptions;
 
  public class MainActivity extends FragmentActivity implements OnMapReadyCallback,
          GoogleApiClient.ConnectionCallbacks,
          GoogleApiClient.OnConnectionFailedListener,
          LocationListener, GoogleMap.InfoWindowAdapter, GoogleMap.OnInfoWindowClickListener {
-
+       //  AlertDialog.Builder alerta = new AlertDialog.Builder(this);
 
      private GoogleMap mMap;
     @Override
@@ -104,7 +107,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
     }
 
      @Override
-     public void onMapReady(GoogleMap googleMap) {
+     public void onMapReady(final GoogleMap googleMap) {
          mMap=googleMap;
          mMap.setMapType(googleMap.MAP_TYPE_HYBRID);
          mMap.setMyLocationEnabled(true);
@@ -127,13 +130,45 @@ import com.google.android.gms.maps.model.MarkerOptions;
          }
 
 
-         mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener(){
+        /* mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener(){
              @Override
              public void onMapClick(LatLng latLng) {
                  //muestra coordenadas de posicion tocada dentro del mapa
                  Toast.makeText(getApplicationContext(), latLng.toString(), Toast.LENGTH_LONG).show();
+
              }
          });
+         */
+         Toast.makeText(this, "Manten presionado en un lugar del mapa para agregar marcador", Toast.LENGTH_LONG).show();
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener(){
+            AlertDialog.Builder alerta = new AlertDialog.Builder(MainActivity.this);
+             @Override
+             public void onMapLongClick(final LatLng latLng) {
+                 //muestra coordenadas de posicion tocada dentro del mapa
+                 //Toast.makeText(getApplicationContext(), latLng.toString(), Toast.LENGTH_LONG).show();
+                 alerta.setTitle("Nuevo marcador");
+                 alerta.setMessage("Crear nuevo marcador aqui? ");
+                 alerta.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+                     public void onClick(DialogInterface dialog, int which) {
+                         MarkerOptions markerOptions = new MarkerOptions();
+                         markerOptions.title("marcador con dialogo de alerta");
+                         markerOptions.position(latLng);
+                         mMap.addMarker(markerOptions);
+                         Log.e("coordenadas: ",latLng.toString());
+                     }
+                 })
+                         .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                             public void onClick(DialogInterface dialog, int which) {
+                                 dialog.cancel();
+                             }
+                         })
+                         .setIcon(android.R.drawable.ic_dialog_info)
+                         .show();
+
+
+             }
+         });
+
 
          LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
          locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1, 1, this);
@@ -145,21 +180,16 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
      public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
      public boolean checkLocationPermission(){
-
          if (ContextCompat.checkSelfPermission(this,
                  Manifest.permission.ACCESS_FINE_LOCATION)
                  != PackageManager.PERMISSION_GRANTED) {
-
              //preguntar si el usuario necesita explicacion
              if (ActivityCompat.shouldShowRequestPermissionRationale(this,
                      Manifest.permission.ACCESS_FINE_LOCATION)) {
-
                  // Mostrar explicacion del permiso
                  ActivityCompat.requestPermissions(this,
                          new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                          MY_PERMISSIONS_REQUEST_LOCATION);
-
-
              } else {
                  // pedir el permiso
                  ActivityCompat.requestPermissions(this,
@@ -185,24 +215,20 @@ import com.google.android.gms.maps.model.MarkerOptions;
                           if (ContextCompat.checkSelfPermission(this,
                                   Manifest.permission.ACCESS_FINE_LOCATION)
                                   == PackageManager.PERMISSION_GRANTED) {
-
-
                               mMap.setMyLocationEnabled(true);
                           }
-
                       } else {
-
                           // Permiso denegado
                           Toast.makeText(this, "Permiso denegado", Toast.LENGTH_LONG).show();
                       }
                       return;
                   }
-
                   // agregar mas case para mas permisos(wifi,contactos,etc)
 
               }
           }catch (Exception e){
               e.printStackTrace();
+
           }
      }
      @Override
@@ -223,18 +249,19 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
      @Override
      public void onLocationChanged(Location location) {
-         mMap.clear();
+        /* mMap.clear();
 
          LatLng latLng = new LatLng(location.getLatitude(),location.getLongitude());
 
        MarkerOptions markerOptions = new MarkerOptions();
          markerOptions.position(latLng);
          markerOptions.title("Mi lugar");
-         //adding marker to the map
+         //agregar marcador al mapa
          mMap.addMarker(markerOptions);
 
-         //opening position with some zoom level in the map
+         //abrir en la posicion del marcador con zoom
          mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f));
+         */
      }
 
      public void BotonMarcador(Location location){
@@ -274,7 +301,8 @@ import com.google.android.gms.maps.model.MarkerOptions;
          View view = inflater.inflate(R.layout.info_windows,null,false);
          view.findViewById(R.id.txtTitulo);
          view.findViewById(R.id.txtDescripcion);
-         view.findViewById(R.id.txtDescripcion);
+         view.findViewById(R.id.txtDireccion);
+         view.findViewById(R.id.imagen);
          return  view;
      }
 
